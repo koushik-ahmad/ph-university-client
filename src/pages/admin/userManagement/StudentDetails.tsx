c5728d5726e6e210fcfc283987fd0a4224041c16
Koushik Ahmad <koushikahmad123@gmail.com>
2024-02-25 22:15:10 +0600

    first commit

---
+++ src/pages/admin/userManagement/StudentDetails.tsx
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+
+const StudentDetails = () => {
+  const { studentId } = useParams();
+  console.log(studentId);
+
+  return (
+    <div>
+      <h1>This is Student Details of {studentId}</h1>
+    </div>
+  );
+};
+
+export default StudentDetails;
